24e7936e0f204409de89f0a860c6cca8e06f0624
Harshit-Banwal <harshitbanwal849@gmail.com>
2024-04-25 16:14:45 +0530

    revert bootstrap and backend file

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:5000';
-// export const API = 'https://shop-now-api.vercel.app';
+// export const API = 'http://localhost:5000';
+export const API = 'https://shop-now-api.vercel.app';
